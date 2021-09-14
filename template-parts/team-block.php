e66cc62d3c7e67ab295d93c1c87d3ff72c5bbdda
Ruvick <rudikov.web@ya.ru>
2021-09-14 23:20:35 +0300

    Блок Команда в темплейт

---
+++ template-parts/team-block.php
@@ -0,0 +1,67 @@
+<div class="team__row d-flex">
+
+	<div class="team__card">
+		<span class="team__card-sticker"></span>
+		<div class="team__card-img">
+			<img src="<?php echo get_template_directory_uri();?>/img/team/01.jpg" alt="">
+		</div>
+		<div class="team__card-descp">
+			<h4>
+				Анна <br>
+				Исаева
+			</h4>
+			<p class="team__card-experience">Стаж работы: 4 года </p>
+			<a href="tel:79009009988" class="team__card-tel">+ 7 900 900 99 88</a>
+			<a href="mailto:info@kurskaya-nedvigimost.ru" class="team__card-email">info@kurskaya-nedvigimost.ru</a>
+		</div>
+	</div>
+
+	<div class="team__card">
+		<!-- <span class="team__card-sticker"></span> -->
+		<div class="team__card-img">
+			<img src="<?php echo get_template_directory_uri();?>/img/team/02.jpg" alt="">
+		</div>
+		<div class="team__card-descp">
+			<h4>
+				Светлана <br>
+				Князева
+			</h4>
+			<p class="team__card-experience">Стаж работы: 4 года </p>
+			<a href="tel:79009009988" class="team__card-tel">+ 7 900 900 99 88</a>
+			<a href="mailto:info@kurskaya-nedvigimost.ru" class="team__card-email">info@kurskaya-nedvigimost.ru</a>
+		</div>
+	</div>
+
+	<div class="team__card">
+		<!-- <span class="team__card-sticker"></span> -->
+		<div class="team__card-img">
+			<img src="<?php echo get_template_directory_uri();?>/img/team/03.jpg" alt="">
+		</div>
+		<div class="team__card-descp">
+			<h4>
+				Инга <br>
+				Власова
+			</h4>
+			<p class="team__card-experience">Стаж работы: 4 года </p>
+			<a href="tel:79009009988" class="team__card-tel">+ 7 900 900 99 88</a>
+			<a href="mailto:info@kurskaya-nedvigimost.ru" class="team__card-email">info@kurskaya-nedvigimost.ru</a>
+		</div>
+	</div>
+
+	<div class="team__card">
+		<!-- <span class="team__card-sticker"></span> -->
+		<div class="team__card-img">
+			<img src="<?php echo get_template_directory_uri();?>/img/team/04.jpg" alt="">
+		</div>
+		<div class="team__card-descp">
+			<h4>
+				Cергей <br>
+				Иванов
+			</h4>
+			<p class="team__card-experience">Стаж работы: 4 года </p>
+			<a href="tel:79009009988" class="team__card-tel">+ 7 900 900 99 88</a>
+			<a href="mailto:info@kurskaya-nedvigimost.ru" class="team__card-email">info@kurskaya-nedvigimost.ru</a>
+		</div>
+	</div>
+
+</div>
