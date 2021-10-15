99580082c0badc16970f57bdc6bb1ccd402dc568
asmi046 <asmi046@gmail.com>
2021-10-15 15:25:10 +0300

    Вывод картинки

--- page-cat.php
+++ page-cat.php
@@ -42,116 +42,18 @@ get_header(); ?>
                         <?
                             global $wpdb;
 
-                            $object = $wpdb->get_results( "SELECT id FROM `kn_objnedv`" );
+                            $object = $wpdb->get_results( "SELECT * FROM `kn_objnedv`" );
                         ?>
                         
-                        <div class="hot-deals__card">
-							<div class="hot-deals__card-img">
-								<img src="<?php echo get_template_directory_uri();?>/img/product/01.jpg" alt="">
-							</div>
-							<div class="hot-deals__card-descp">
-								<p class="hot-deals__card-price rub">2 500 000 </p>
-								<div class="hot-deals__card-charect d-flex">
-									<p class="hot-deals__card-housing">2-х комнатная квартира </p>
-									<p class="hot-deals__card-amount">44 м² | 5 / 5 эт.</p>
-								</div>
-								<p class="hot-deals__card-address">ул. Новая д. 34</p>
-							</div>
-							<div class="hot-deals__card-btn d-flex">
-								<a href="#" class="hot-deals__card-link">Подробнее</a>
-								<a href="#" class="hot-deals__card-link">Оставить заявку</a>
-							</div>
-						</div>
 
-						<div class="hot-deals__card">
-							<div class="hot-deals__card-img">
-								<img src="<?php echo get_template_directory_uri();?>/img/product/02.jpg" alt="">
-							</div>
-							<div class="hot-deals__card-descp">
-								<p class="hot-deals__card-price rub">2 500 000 </p>
-								<div class="hot-deals__card-charect d-flex">
-									<p class="hot-deals__card-housing">2-х комнатная квартира </p>
-									<p class="hot-deals__card-amount">44 м² | 5 / 5 эт.</p>
-								</div>
-								<p class="hot-deals__card-address">ул. Новая д. 34</p>
-							</div>
-							<div class="hot-deals__card-btn d-flex">
-								<a href="#" class="hot-deals__card-link">Подробнее</a>
-								<a href="#" class="hot-deals__card-link">Оставить заявку</a>
-							</div>
-						</div>
 
-						<div class="hot-deals__card">
-							<div class="hot-deals__card-img">
-								<img src="<?php echo get_template_directory_uri();?>/img/product/03.jpg" alt="">
-							</div>
-							<div class="hot-deals__card-descp">
-								<p class="hot-deals__card-price rub">2 500 000 </p>
-								<div class="hot-deals__card-charect d-flex">
-									<p class="hot-deals__card-housing">2-х комнатная квартира </p>
-									<p class="hot-deals__card-amount">44 м² | 5 / 5 эт.</p>
-								</div>
-								<p class="hot-deals__card-address">ул. Новая д. 34</p>
-							</div>
-							<div class="hot-deals__card-btn d-flex">
-								<a href="#" class="hot-deals__card-link">Подробнее</a>
-								<a href="#" class="hot-deals__card-link">Оставить заявку</a>
-							</div>
-						</div>
 
-						<div class="hot-deals__card">
-							<div class="hot-deals__card-img">
-								<img src="<?php echo get_template_directory_uri();?>/img/product/04.jpg" alt="">
-							</div>
-							<div class="hot-deals__card-descp">
-								<p class="hot-deals__card-price rub">2 500 000 </p>
-								<div class="hot-deals__card-charect d-flex">
-									<p class="hot-deals__card-housing">2-х комнатная квартира </p>
-									<p class="hot-deals__card-amount">44 м² | 5 / 5 эт.</p>
-								</div>
-								<p class="hot-deals__card-address">ул. Новая д. 34</p>
-							</div>
-							<div class="hot-deals__card-btn d-flex">
-								<a href="#" class="hot-deals__card-link">Подробнее</a>
-								<a href="#" class="hot-deals__card-link">Оставить заявку</a>
-							</div>
-						</div>
+                        <?
+                            foreach ($object as $elem) {
+                                get_template_part('template-parts/objec', 'elem', ["elem" => $elem]);
+                            }
+                        ?>
 
-						<div class="hot-deals__card">
-							<div class="hot-deals__card-img">
-								<img src="<?php echo get_template_directory_uri();?>/img/product/05.jpg" alt="">
-							</div>
-							<div class="hot-deals__card-descp">
-								<p class="hot-deals__card-price rub">2 500 000 </p>
-								<div class="hot-deals__card-charect d-flex">
-									<p class="hot-deals__card-housing">2-х комнатная квартира </p>
-									<p class="hot-deals__card-amount">44 м² | 5 / 5 эт.</p>
-								</div>
-								<p class="hot-deals__card-address">ул. Новая д. 34</p>
-							</div>
-							<div class="hot-deals__card-btn d-flex">
-								<a href="#" class="hot-deals__card-link">Подробнее</a>
-								<a href="#" class="hot-deals__card-link">Оставить заявку</a>
-							</div>
-						</div>
-
-						<div class="hot-deals__card">
-							<div class="hot-deals__card-img">
-								<img src="<?php echo get_template_directory_uri();?>/img/product/06.jpg" alt="">
-							</div>
-							<div class="hot-deals__card-descp">
-								<p class="hot-deals__card-price rub">2 500 000 </p>
-								<div class="hot-deals__card-charect d-flex">
-									<p class="hot-deals__card-housing">2-х комнатная квартира </p>
-									<p class="hot-deals__card-amount">44 м² | 5 / 5 эт.</p>
-								</div>
-								<p class="hot-deals__card-address">ул. Новая д. 34</p>
-							</div>
-							<div class="hot-deals__card-btn d-flex">
-								<a href="#" class="hot-deals__card-link">Подробнее</a>
-								<a href="#" class="hot-deals__card-link">Оставить заявку</a>
-							</div>
-						</div>
 					</div>
 
 					<div class="pagging">
---
+++ template-parts/objec-elem.php
@@ -0,0 +1,18 @@
+<div class="hot-deals__card">
+<?php print_r($args['elem']); ?>	
+<div class="hot-deals__card-img">
+		<img src="<?php echo $args['elem']->photo;?>" alt="">
+	</div>
+	<div class="hot-deals__card-descp">
+		<p class="hot-deals__card-price rub">2 500 000 </p>
+		<div class="hot-deals__card-charect d-flex">
+			<p class="hot-deals__card-housing">2-х комнатная квартира </p>
+			<p class="hot-deals__card-amount">44 м² | 5 / 5 эт.</p>
+		</div>
+		<p class="hot-deals__card-address">ул. Новая д. 34</p>
+	</div>
+	<div class="hot-deals__card-btn d-flex">
+		<a href="#" class="hot-deals__card-link">Подробнее</a>
+		<a href="#" class="hot-deals__card-link">Оставить заявку</a>
+	</div>
+</div>
