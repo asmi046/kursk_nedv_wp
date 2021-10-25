3dafcee169758ee5e0063cc931a37c44e07a322c
Ruvick <rudikov.web@ya.ru>
2021-10-25 16:17:29 +0300

    Кликабельные картинки

--- template-parts/objec-elem.php
+++ template-parts/objec-elem.php
@@ -1,21 +1,21 @@
 <?
-	$adres = $args['elem']->obl;
-	if (!empty($args['elem']->obl_raion)) $adres .= " ".$args['elem']->obl_raion;
-	if (!empty($args['elem']->np)) $adres .= " ".$args['elem']->np;
-	if (!empty($args['elem']->np_raion)) $adres .= " ".$args['elem']->np_raion;
-	if (!empty($args['elem']->street)) $adres .= " ".$args['elem']->street;
-	if (!empty($args['elem']->dom_number)) $adres .= " ".$args['elem']->dom_number;
+$adres = $args['elem']->obl;
+if (!empty($args['elem']->obl_raion)) $adres .= " ".$args['elem']->obl_raion;
+if (!empty($args['elem']->np)) $adres .= " ".$args['elem']->np;
+if (!empty($args['elem']->np_raion)) $adres .= " ".$args['elem']->np_raion;
+if (!empty($args['elem']->street)) $adres .= " ".$args['elem']->street;
+if (!empty($args['elem']->dom_number)) $adres .= " ".$args['elem']->dom_number;
 
-	$sitename = empty($args['elem']->site_name)?$args['elem']->type." ".$args['elem']->street:$args['elem']->site_name;
+$sitename = empty($args['elem']->site_name)?$args['elem']->type." ".$args['elem']->street:$args['elem']->site_name;
 
-	$metazg = $args['elem']->area1.(empty($args['elem']->area2)?"":" / ".$args['elem']->area2);
-	$etagnost = $args['elem']->floor.(empty($args['elem']->floors)?"":" / ".$args['elem']->floors);
+$metazg = $args['elem']->area1.(empty($args['elem']->area2)?"":" / ".$args['elem']->area2);
+$etagnost = $args['elem']->floor.(empty($args['elem']->floors)?"":" / ".$args['elem']->floors);
 ?>
 <div class="hot-deals__card">	
-<div class="hot-deals__card-img" data-lot = "<?echo $args['elem']->lot;?>" data-crmid = "<?echo $args['elem']->row_id;?>">
+	<a href="<?bloginfo("url")?>/obekt/<?echo $args['elem']->row_id;?>" class="hot-deals__card-img" data-lot = "<?echo $args['elem']->lot;?>" data-crmid = "<?echo $args['elem']->row_id;?>">
 		<img src="<?php echo (empty($args['elem']->photo))?get_bloginfo("template_url")."/img/no-photo.jpg":$args['elem']->photo;?>" alt="">
-	</div>
-	<div class="hot-deals__card-descp">
+	</a> 
+	<div class="hot-deals__card-descp"> 
 		<p class="hot-deals__card-price rub price_formator"><?echo $args['elem']->price;?> </p>
 		<div class="hot-deals__card-charect d-flex">
 			<p class="hot-deals__card-housing"><?echo $sitename;?></p>
