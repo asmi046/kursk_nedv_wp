7edcc1cd69bae8656862274ecb192dbea04fd84d
Ruvick <rudikov.web@ya.ru>
2021-11-02 21:23:18 +0300

    Убрал ошибки

--- functions.php
+++ functions.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
 define("COMPANY_NAME", "КУРСКАЯ НЕДВИЖИМОСТЬ");
 define("MAIL_RESEND", "noreply@ultrakresla.ru");
 
