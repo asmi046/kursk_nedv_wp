221233dddd4cb87e58303600955d11a8b3971e1c
asmi046 <asmi046@gmail.com>
2022-05-22 17:54:26 +0300

    Поправил универсальную форму

--- template-parts/form-real-estate.php
+++ template-parts/form-real-estate.php
@@ -7,8 +7,8 @@
 		<div class="headen_form_blk">
 			<form action="#" class="sell-pass-info__form universal_send_form">
 				<div class="form__line">
-					<input autocomplete="off" type="text" name="name" data-error="Заполните поля" data-value="Имя*" class="sell-pass-info__form-input input _req">
-					<input autocomplete="off" type="text" name="tel" data-error="Заполните поля" data-value="Телефон*" class="sell-pass-info__form-input input _phone _req">
+					<input autocomplete="off" type="text" name="name" data-error="Заполните поля" data-value="Имя*" class="sell-pass-info__form-input input _req _name">
+					<input autocomplete="off" type="text" name="tel" data-error="Заполните поля" data-value="Телефон*" class="sell-pass-info__form-input input _phone _req _tel">
 				</div>
 				<button type="button" class="sell-pass-info__form-btn form-btn u_send btn">Получить бесплатную консультацию</button>
 			</form> 
